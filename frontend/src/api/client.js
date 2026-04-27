16403f725accc1c4c32ebc7461fcb2671fcf97ef
Labh Bothra <labhbothra1380@gmail.com>
2026-04-28 01:10:16 +0530

    D

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:2500/api";
+const API_URL = "https://manufacture-erp-backend.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_URL,
